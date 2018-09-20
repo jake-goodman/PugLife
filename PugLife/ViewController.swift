3ebf39003a6d5fbebf1017d48d2a4a126cdbd26e
Jake Goodman <jakemgoodman@gmail.com>
2018-09-20 10:06:21 -0700

    Initial commit

---
+++ PugLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PugLife
+//
+//  Created by Jake Goodman on 9/20/18.
+//  Copyright © 2018 Jake Goodman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
